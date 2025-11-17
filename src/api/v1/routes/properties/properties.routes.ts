a7ef1cc58d0529c13c5c214282b76dac67f713e6
bchurlinov <bchurlinov@hotmail.com>
2025-11-17 18:45:55 +0100

    add validate request

--- src/api/v1/routes/properties/properties.routes.ts
+++ src/api/v1/routes/properties/properties.routes.ts
@@ -1,8 +1,18 @@
 import { Router } from "express"
 import { getPropertiesController } from "@controllers/properties/properties.controller.js"
+import { query} from "express-validator"
+import { validateRequest } from "@middlewares/validate_request.js"
 
 const router = Router()
 
-router.get("/", getPropertiesController)
+router.get(
+    "/",
+    [
+        query("page").optional().isInt({ min: 1 }).withMessage("Page must be a positive integer").toInt(),
+        query("limit").optional().isInt({ min: 1, max: 500 }).withMessage("Limit must be between 1 and 500").toInt(),
+    ],
+    validateRequest,
+    getPropertiesController
+)
 
 export default router
